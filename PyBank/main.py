6aa4a807fbe51c72659f7584c4cb062300111165
Angel Milla <angheljf@gmail.com>
2021-02-21 15:54:23 -0500

    Add Solution for PyPoll

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,14 +3,11 @@ import os
 import csv
 
 # Set variables
-total_months = 0
-total_amount = 0
 monthly_change =[]
 month_count = []
 increase = 0
-increase_month = 0
 decrease = 0
-decrease_month = 0
+
 
 # Set path for file
 csvpath = '../git/python-challenge/PyBank/Resources/budget_data.csv'
@@ -25,33 +22,33 @@ with open (csvpath, newline="") as csvfile:
     # print(f"Header: {csvheader}")
     # print(f"Row: {row}")
     previous_row = int(row[1])
-    total_months += 1
-    total_amount += int(row[1])
+    total_months = 1
+    total_amount = int(row[1])
     increase = int(row[1])
-    increase_month = row[0]
 
     # Iterate each row after the header
     for row in csvreader:
-        
-        total_months += 1 # Calculate number of months
-        total_amount += int(row[1]) # Calculate Net Amount of Profit/Loss
+        # Calculate number of months
+        total_months += 1
+        # Calculate Net Amount of Profit/Loss
+        total_amount += int(row[1]) 
         # Month to month change
         revenue_change = int(row[1]) - previous_row
         monthly_change.append(revenue_change)
         previous_row = int(row[1])
         month_count.append(row[0])
-
+        # Find month of greateast increase and decrease
         if int(row[1]) > increase:
             increase = int(row[1])
             increase_month = row[0]
         if int(row[1]) < decrease:
             decrease = int(row[1])
             decrease_month = row[0]
-    average = sum(monthly_change) / len(monthly_change)
+    average = sum(monthly_change) / len(month_count)
     highest = max(monthly_change)
     lowest = min(monthly_change)
 
-# Print Analysis
+# Analysis
 print(f"Financial Analysis")
 print(f"---------------------------")
 print(f"Total Months: {total_months}")
@@ -60,18 +57,17 @@ print(f"Average Change: ${average:.2f}")
 print(f"Greatest Increase in Profits:, {increase_month}, (${highest})")
 print(f"Greatest Decrease in Profits:, {decrease_month}, (${lowest})") 
 
-# Specify File To Write To
+# Create new text file of Analysis
 output_file = '../git/python-challenge/PyBank/Analysis/budget_analysis.txt'
 
-# Open File Using "Write" Mode. Specify The Variable To Hold The Contents
 with open(output_file, 'w',) as txtfile:
 
-# Write New Data
+# Write Data
     txtfile.write(f"Financial Analysis\n")
     txtfile.write(f"---------------------------\n")
     txtfile.write(f"Total Months: {total_months}\n")
     txtfile.write(f"Total: ${total_amount}\n")
-    txtfile.write(f"Average Change: ${average}\n")
+    txtfile.write(f"Average Change: ${average:.2f}\n")
     txtfile.write(f"Greatest Increase in Profits:, {increase_month}, (${highest})\n")
     txtfile.write(f"Greatest Decrease in Profits:, {decrease_month}, (${lowest})\n")
     
---
+++ PyPoll/main.py
@@ -0,0 +1,82 @@
+# Import Modules
+import os
+import csv
+
+# Set Variables
+total_votes = 0
+khan_votes = 0
+correy_votes = 0
+li_votes = 0
+otooley_votes = 0
+
+# Set path for file
+csvpath = '../git/python-challenge/PyPoll/Resources/election_data.csv'
+# Open and Read file
+with open (csvpath, newline="") as csvfile:
+    # csvreader set variable and delimiter for data
+    csvreader = csv.reader(csvfile, delimiter=",")
+    csvheader = next(csvreader)
+    # Read the header and the first row to check data 
+    # print(f"Header: {csvheader}")
+    
+    for row in csvreader:
+        # Calculate total votes
+        total_votes += 1
+        # Calculate votes for each candidate
+        if (row[2] == "Khan"):
+            khan_votes += 1
+        elif (row[2] == "Correy"):
+            correy_votes += 1
+        elif (row[2] == "Li"):
+            li_votes += 1
+        else:
+            otooley_votes += 1
+
+# Calculate % of vote for each candidate
+khan_percent = khan_votes / total_votes
+correy_percent = correy_votes / total_votes
+li_percent = li_votes / total_votes
+otooley_percent = otooley_votes / total_votes
+
+# Find out who won
+winner = max(khan_votes, correy_votes, li_votes, otooley_votes)
+
+if winner == khan_votes:
+    winner_name = "Khan"
+elif winner == correy_votes:
+    winner_name = "Correy"
+elif winner == li_votes:
+    winner_name = "Li"
+else:
+    winner_name = "O'Tooley"
+
+# Analysis
+print(f"Election Results")
+print(f"---------------------------")
+print(f"Total Votes: {total_votes}")
+print(f"---------------------------")
+print(f"Kahn: {khan_percent:.3%}({khan_votes})")
+print(f"Correy: {correy_percent:.3%}({correy_votes})")
+print(f"Li: {li_percent:.3%}({li_votes})")
+print(f"O'Tooley: {otooley_percent:.3%}({otooley_votes})")
+print(f"---------------------------")
+print(f"Winner: {winner_name}")
+print(f"---------------------------")
+
+# Create new text file of Analysis
+output_file = '../git/python-challenge/PyPoll/Analysis/poll_analysis.txt'
+
+with open(output_file, 'w',) as txtfile:
+
+# Write Data
+    txtfile.write(f"Election Results\n")
+    txtfile.write(f"---------------------------\n")
+    txtfile.write(f"Total Votes: {total_votes}\n")
+    txtfile.write(f"---------------------------\n")
+    txtfile.write(f"Kahn: {khan_percent:.3%}({khan_votes})\n")
+    txtfile.write(f"Correy: {correy_percent:.3%}({correy_votes})\n")
+    txtfile.write(f"Li: {li_percent:.3%}({li_votes})\n")
+    txtfile.write(f"O'Tooley: {otooley_percent:.3%}({otooley_votes})\n")
+    txtfile.write(f"---------------------------\n")
+    txtfile.write(f"Winner: {winner_name}\n")
+    txtfile.write(f"---------------------------\n")
